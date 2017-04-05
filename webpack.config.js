640f8cdadd1282ca6c8405ee8ab45f6a01bf9db6
w <w@w.com>
2017-04-05 17:46:00 +0800

    fix on windows 10

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
-const ngtools = require('@ngtools/webpack');
-
+const ngToolsWebpack = require('@ngtools/webpack');
+// workaround https://github.com/angular/angular-cli/issues/5329
+ path= require("path");
+var aotPlugin = new ngToolsWebpack.AotPlugin({
+    tsConfigPath: "./tsconfig.json"
+});
+aotPlugin._compilerHost._resolve = function(path_to_resolve) {
+    path_1 = require("path");
+    path_to_resolve = aotPlugin._compilerHost._normalizePath(path_to_resolve);
+    if (path_to_resolve[0] == '.') {
+        return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost.getCurrentDirectory(), path_to_resolve));
+    }
+    else if (path_to_resolve[0] == '/' || path_to_resolve.match(/^\w:\//)) {
+        return path_to_resolve;
+    }
+    else {
+        return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost._basePath, path_to_resolve));
+    }
+};
 module.exports = {
 	entry: {
 		main: './src/main.server.ts'
@@ -13,9 +30,7 @@ module.exports = {
 		filename: '[name].js'
 	},
 	plugins: [
-		new ngtools.AotPlugin({
-			tsConfigPath: './tsconfig.json',
-		})
+	aotPlugin
 	],
 	module: {
 		rules: [
